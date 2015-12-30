4db2dd4b57195d79343044d1d6aa67e50aa33f43
Thalur <Thalur@users.noreply.github.com>
2015-12-30 14:37:15 +0100

    [DEV] Fixed OSX build

--- src/client/gfx/oglincludes.h
+++ src/client/gfx/oglincludes.h
@@ -1,13 +1,15 @@
 #ifdef AK_SYSTEM_ANDROID
-#define GL_GLEXT_PROTOTYPES
-#include <EGL/egl.h>
-#include <GLES/gl.h>
-#include <GLES/glext.h>
-#elif defined AK_SYSTEM_OSX
-#include "OpenGL/gl.h"
-#include "OpenGL/glext.h"
+  #define GL_GLEXT_PROTOTYPES
+  #include <EGL/egl.h>
+  #include <GLES/gl.h>
+  #include <GLES/glext.h>
 #else
-#define FREEGLUT_LIB_PRAGMAS 0
-#include "GL/glew.h"
-#include "GL/freeglut.h"
+  #define FREEGLUT_LIB_PRAGMAS 0
+  #include "GL/glew.h"
+  #include "GL/freeglut.h"
+  #ifdef AK_SYSTEM_OSX
+    #include "OpenGL/gl.h"
+    #include "OpenGL/glext.h"
+  #endif
+
 #endif
